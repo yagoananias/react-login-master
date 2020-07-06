07da356fcc401231728e9e42676738437b7a19f6
Yago Ananias <yago_souza92@yahoo.com.br>
2020-07-06 15:03:21 -0300

    Initial State

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UIButton from 'components/UI/Button/Button';
 
 import './Login.css';
 
+function initialState() {
+  return { user: '', password: ''}
+}
+
 const UserLogin = () => {
+  const [values, setValues] = useState(initialState)
+
+  function onChange(event) {
+    const { value, name } = event.target
+
+    setValues({
+      ...values,
+      [name]: value,
+    })
+  }
+
   return (
     <div className="user-login">
       <h1 className="user-login__title">Acessar o Sistema</h1>
       <form autoComplete="nope">
         <div className="user-login__form-control">
           <label htmlFor="email">E-mail</label>
-          <input id="email" type="text" name="email" autoComplete="off" />
+          <input
+            id="user"
+            type="text"
+            name="user"
+            autoComplete="off"
+            onChange={onChange}
+            value={values.user} />
         </div>
         <div className="user-login__form-control">
           <label htmlFor="password">Senha</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={onChange}
+            value={values.password} />
         </div>
         <UIButton
           type="submit"
